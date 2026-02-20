70792a393dabc87babec2f2b3a2db1923f280c73
siddharthshankarchoudhary <shankarchoudharysiddharth@gmail.com>
2026-02-20 11:24:19 +0530

    Initialize project

---
+++ src/main/java/io/github/siddharthshankarchoudhary/cachedcalculator/CachedCalculatorApplication.java
@@ -0,0 +1,13 @@
+package io.github.siddharthshankarchoudhary.cachedcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CachedCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CachedCalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/siddharthshankarchoudhary/cachedcalculator/CachedCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.siddharthshankarchoudhary.cachedcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CachedCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
