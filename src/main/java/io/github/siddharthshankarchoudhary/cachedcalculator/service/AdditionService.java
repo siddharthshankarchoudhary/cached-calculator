26fdd64d61c07349bcbf7cd4171e34d15fcc047c
siddharthshankarchoudhary <shankarchoudharysiddharth@gmail.com>
2026-02-20 15:57:53 +0530

    Remove logs

--- src/main/java/io/github/siddharthshankarchoudhary/cachedcalculator/service/AdditionService.java
+++ src/main/java/io/github/siddharthshankarchoudhary/cachedcalculator/service/AdditionService.java
@@ -18,12 +18,9 @@ public class AdditionService {
     public double execute(double num1, double num2) {
         String cacheKey = String.format("%s %s %s", CalculatorCacheKey.ADD, num1, num2);
         if(calculatorCache.containsKey(cacheKey)) {
-            double cachedValue = calculatorCache.get(cacheKey);
-            System.out.println("Cached value is : " + cachedValue);
-            return cachedValue;
+            return calculatorCache.get(cacheKey);
         } else {
             double sum = add(num1, num2);
-            System.out.println("Calculated value is : " + sum);
             calculatorCache.put(cacheKey, sum);
             return sum;
         }
